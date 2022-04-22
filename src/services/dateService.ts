9d16e6cb12b9f6c2b7eeffb41f805e05d038826b
jiyuujin <jiyuujinunite@gmail.com>
2022-04-22 18:44:32 +0900

    refactor: isSpecialDay

--- src/services/dateService.ts
+++ src/services/dateService.ts
@@ -36,14 +36,25 @@ export const isBusinessDay = (date) => {
     return false
   }
 
-  if (date.getFullYear() == 2021 && date.getMonth() + 1 == 12 && date.getDate() > 28) {
-    return false
-  }
-  if (date.getFullYear() == 2022 && date.getMonth() + 1 == 1 && date.getDate() < 5) {
+  if (isSpecialDay(date) == false) {
     return false
   }
 
   const calJa = CalendarApp.getCalendarById(JP_CAL)
 
   return calJa.getEventsForDay(date).length <= 0
+}
+
+/**
+ * 特別対応
+ * 2021-2022 年末年始
+ */
+export const isSpecialDay = (date) => {
+  if (date.getFullYear() == 2021 && date.getMonth() + 1 == 12 && date.getDate() > 28) {
+    return false
+  }
+  if (date.getFullYear() == 2022 && date.getMonth() + 1 == 1 && date.getDate() < 5) {
+    return false
+  }
+  return true
 }
